210e8716774d85d1f47df12766f8e22c716e93a7
John Doe <john@sample.com>
2019-02-23 20:55:59 +0530

    Enabling Test

--- src/test/java/test/ApiTest.java
+++ src/test/java/test/ApiTest.java
@@ -5,7 +5,7 @@ import clients.ApiAction;
 import clients.models.response.BasicAuthResponse;
 
 public class ApiTest {
-  @Test(enabled= false)
+  @Test()
   public void apiTest() {
       ApiAction apiAction = new ApiAction();
       BasicAuthResponse response = apiAction.getBasicPostman("/basic-auth");
